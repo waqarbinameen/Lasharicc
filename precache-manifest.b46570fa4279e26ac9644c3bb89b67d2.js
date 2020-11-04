5aa6c147a5695e97783d531e0913c135284de784
waqarbinameen <waqar.ch.ahmad@gmail.com>
2020-11-04 11:58:05 +0500

    Updates

---
+++ precache-manifest.b46570fa4279e26ac9644c3bb89b67d2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae0ddcf7ae728aeb50fbd68bdd0d41f0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2b1b12c5d6dc78995e29",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "e9296d8ca6b76fdc88a2",
+    "url": "/static/css/main.2b0f4634.chunk.css"
+  },
+  {
+    "revision": "2b1b12c5d6dc78995e29",
+    "url": "/static/js/2.9edc0ae1.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9edc0ae1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e9296d8ca6b76fdc88a2",
+    "url": "/static/js/main.4d535ea5.chunk.js"
+  },
+  {
+    "revision": "9fd8555fa6adf723745a",
+    "url": "/static/js/runtime-main.b2593cbe.js"
+  }
+]);
